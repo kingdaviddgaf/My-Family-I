51bfa20511d651926275603a0a002010754cf90b
kingdaviddgaf <ibekweibekwe6@gmail.com>
2026-06-03 02:03:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 app.get("/", (req, res) => {
   res.send(`
   <!DOCTYPE html>
