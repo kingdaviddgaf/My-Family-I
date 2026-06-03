0dbdb8560c6dee00f917a0f22021e6f27fa15a47
kingdaviddgaf <ibekweibekwe6@gmail.com>
2026-06-03 20:28:11 +0100

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+
+  content: {
+    type: String,
+    required: true
+  }
+
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Post", PostSchema);
