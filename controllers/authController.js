5100a4e582dd8a099b21b32d38938e8bc9a664e2
kingdaviddgaf <ibekweibekwe6@gmail.com>
2026-06-03 02:52:51 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,12 @@ exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.send("Email already exists");
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
@@ -15,14 +21,8 @@ exports.register = async (req, res) => {
 
     await user.save();
 
-    res.json({
-      success: true,
-      message: "User registered successfully"
-    });
+    res.send("Account created successfully!");
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message
-    });
+    res.send("Registration failed");
   }
 };
