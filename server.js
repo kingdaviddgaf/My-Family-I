e59e7fd7222cecba1a026fd6e11d517ab25a5803
kingdaviddgaf <ibekweibekwe6@gmail.com>
2026-06-03 02:09:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
