90caa23b86adb42d13b6e02811d9845a11b643fb
kingdaviddgaf <ibekweibekwe6@gmail.com>
2026-06-05 09:57:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 console.log("MY FAMILY APP STARTING...");
 const app = express();
 const authRoutes = require("./routes/auth");
